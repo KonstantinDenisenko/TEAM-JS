1bd7821412531cbd286d505e0eaf046409b75584
KonstantinDenisenko <konstantin.denisenko91@gmail.com>
2019-03-11 17:15:07 +0200

    js

--- Barley-break/js/main.js
+++ Barley-break/js/main.js
@@ -1,21 +1,21 @@
-var games = document.querySelectorAll(".game");
-var barleyBreak = new Barleybreak(games[0]);
+var game = document.getElementById("game");
+var barleyBreak = new Barleybreak(game);
 barleyBreak.addEvents();
 
 function Barleybreak(game) {
-	this.elements = game.querySelectorAll(".element");
-	this.reset = game.querySelector(".reset");
-	this.cover = game.querySelector(".cover");
-	this.congratulation = game.querySelector(".congratulation");
-	this.steps = game.querySelector(".steps");
-	this.sumSteps = 0;
+	var elements = game.querySelectorAll(".element");
+	var reset = game.querySelector(".reset");
+	var cover = game.querySelector(".cover");
+	var congratulation = game.querySelector(".congratulation");
+	var steps = game.querySelector(".steps");
+	var sumSteps = 0;
 
 	this.addEvents = function () {
-		for(var a = 0; a < this.elements.length; a++){
+		for(var a = 0; a < elements.length; a++){
 			(function (a) {
-				this.elements[a].addEventListener("click", function (e) {
+				elements[a].addEventListener("click", function () {
 					this.neighborCheck(a);
-					this.sumSteps++;
+					sumSteps++;
 				}.bind(this));
 			}.bind(this))(a);
 		}
@@ -24,73 +24,75 @@ function Barleybreak(game) {
 			this.mixCells();
 		}.bind(this));
 
-		this.reset.addEventListener("click", function () {
+		reset.addEventListener("click", function () {
 			this.mixCells();
 		}.bind(this));
 
-		this.congratulation.addEventListener("click", function () {
-			this.cover.classList.remove("active");
-			this.congratulation.classList.remove("active");
+		congratulation.addEventListener("click", function () {
+			cover.classList.remove("active");
+			congratulation.classList.remove("active");
 		}.bind(this));
 	};
 
 	 this.mixCells = function() {
-		this.sumSteps = 0;
+		sumSteps = 0;
 
-		for(var a = 0; a < this.elements.length; a++){
-			if(this.elements[a].innerText === ""){
-				this.elements[a].classList.remove("empty");
+		for(var a = 0; a < elements.length; a++){
+			if(elements[a].innerText === ""){
+				elements[a].classList.remove("empty");
 			}
 		}
 		this.arrayCells = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,""];
 
-		function compareRandom(a, b) {
-			return Math.random() - 0.5;
-		}
-		this.arrayCells.sort(compareRandom);
+		this.arrayCells.sort(this.compareRandom);
 
 		for(var i = 0; i < this.arrayCells.length; i++){
-			this.elements[i].innerText = this.arrayCells[i];
+			elements[i].innerText = this.arrayCells[i];
 		}
 
-		for(var q = 0; q < this.elements.length; q++){
-			if(this.elements[q].innerText === ""){
-				this.elements[q].classList.toggle("empty");
+		for(var q = 0; q < elements.length; q++){
+			if(elements[q].innerText === ""){
+				elements[q].classList.add("empty");
 			}
 		}
 	};
 
+    this.compareRandom = function() {
+        return Math.random() - 0.5;
+    };
+
 	this.neighborCheck = function(index) {
-		if(this.elements[index + 1] && this.elements[index + 1].innerText === ""){
+		if(elements[index + 1] && elements[index + 1].innerText === ""){
 			this.moveCell(index + 1, index);
-		}else if(this.elements[index - 1] && this.elements[index - 1].innerText === ""){
+		}else if(elements[index - 1] && elements[index - 1].innerText === ""){
 			this.moveCell(index - 1, index);
-		}else if(this.elements[index + 4] && this.elements[index + 4].innerText === ""){
+		}else if(elements[index + 4] && elements[index + 4].innerText === ""){
 			this.moveCell(index + 4, index);
-		}else if(this.elements[index - 4] && this.elements[index - 4].innerText === ""){
+		}else if(elements[index - 4] && elements[index - 4].innerText === ""){
 			this.moveCell(index - 4, index);
 		}
 		this.checkSequence();
 	};
 
 	this.moveCell = function (neighboringIndex, currenIndex) {
-		this.elements[neighboringIndex].classList.remove("empty");
-		this.elements[neighboringIndex].innerText = this.elements[currenIndex].innerText;
-		this.elements[currenIndex].innerText = "";
-		this.elements[currenIndex].classList.add("empty");
+		elements[neighboringIndex].classList.remove("empty");
+		elements[neighboringIndex].innerText = elements[currenIndex].innerText;
+		elements[currenIndex].innerText = "";
+		elements[currenIndex].classList.add("empty");
 	};
 
 	this.checkSequence = function() {
-		this.sequence = "123456789101112131415";
+		this.winSequence = "123456789101112131415";
+		this.winSequenceToo = "159132610143711154812";
 		this.currentArrayCells = "";
 
-		for(var a = 0; a < this.elements.length; a++){
-			this.currentArrayCells += this.elements[a].innerText;
+		for(var a = 0; a < elements.length; a++){
+			this.currentArrayCells += elements[a].innerText;
 		}
-		if(this.currentArrayCells === this.sequence){
-			this.cover.classList.add("active");
-			this.congratulation.classList.add("active");
-			this.steps.innerText = this.sumSteps;
+		if(this.currentArrayCells === this.winSequence || this.currentArrayCells === this.winSequenceToo){
+			cover.classList.add("active");
+			congratulation.classList.add("active");
+			steps.innerText = sumSteps;
 		}
 	};
 }
