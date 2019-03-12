cc0fdeabcc4de6b5e23dc703eb4f6a64e2eda3cd
KonstantinDenisenko <konstantin.denisenko91@gmail.com>
2019-03-12 21:54:22 +0200

    js

--- Barley-break/js/main.js
+++ Barley-break/js/main.js
@@ -9,17 +9,66 @@ function Barleybreak(game) {
 	var steps = game.querySelector(".steps");
 	var sumSteps = 0;
     var neighborCheck = function(index) {
-        if(this.verification(index + 1)){
-            this.moveCell(index + 1, index);
-        }else if(this.verification(index - 1)){
-            this.moveCell(index - 1, index);
-        }else if(this.verification(index + 4)){
-            this.moveCell(index + 4, index);
-        }else if(this.verification(index - 4)){
-            this.moveCell(index - 4, index);
+        if(verification(index + 1)){
+            moveCell(index + 1, index);
+        }else if(verification(index - 1)){
+            moveCell(index - 1, index);
+        }else if(verification(index + 4)){
+            moveCell(index + 4, index);
+        }else if(verification(index - 4)){
+            moveCell(index - 4, index);
         }
-        this.checkSequence();
-    }.bind(this);
+        checkSequence();
+    };
+
+    var verification = function(index) {
+        return elements[index] && elements[index].innerText === "";
+    };
+
+    var moveCell = function(neighboringIndex, currenIndex) {
+        elements[neighboringIndex].classList.remove("empty");
+        elements[neighboringIndex].innerText = elements[currenIndex].innerText;
+        elements[currenIndex].innerText = "";
+        elements[currenIndex].classList.add("empty");
+    };
+
+    var checkSequence = function() {
+        var winSequences = ["123456789101112131415", "159132610143711154812"];
+        var currentArrayCells = "";
+
+        for(var a = 0; a < elements.length; a++){
+            currentArrayCells += elements[a].innerText;
+        }
+        if(winSequences.indexOf(currentArrayCells) !== -1){
+            showSuccess();
+        }
+    };
+
+    var showSuccess = function () {
+        cover.classList.add("active");
+        congratulation.classList.add("active");
+        steps.innerText = sumSteps;
+    };
+
+    var mixCells = function() {
+        sumSteps = 0;
+        var a;
+        var arrayCells = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,""];
+
+        arrayCells.sort(compareRandom);
+
+        for(a = 0; a < arrayCells.length; a++){
+            elements[a].innerText = arrayCells[a];
+        }
+
+        for(a = 0; a < elements.length; a++){
+            elements[a].classList.remove("empty");
+            if(elements[a].innerText === ""){
+                elements[a].classList.add("empty");
+            }
+        }
+    };
+
     var compareRandom = function() {
         return Math.random() - 0.5;
     };
@@ -35,61 +84,16 @@ function Barleybreak(game) {
 		}
 
 		window.addEventListener('load', function() {
-			this.mixCells();
-		}.bind(this));
+			mixCells();
+		});
 
 		reset.addEventListener("click", function() {
-			this.mixCells();
-		}.bind(this));
+			mixCells();
+		});
 
 		congratulation.addEventListener("click", function() {
 			cover.classList.remove("active");
 			congratulation.classList.remove("active");
 		});
-	};
-
-	 this.mixCells = function() {
-		sumSteps = 0;
-		var a;
-
-		this.arrayCells = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,""];
-
-		this.arrayCells.sort(compareRandom);
-
-		for(a = 0; a < this.arrayCells.length; a++){
-			elements[a].innerText = this.arrayCells[a];
-		}
-
-		for(a = 0; a < elements.length; a++){
-            elements[a].classList.remove("empty");
-			if(elements[a].innerText === ""){
-				elements[a].classList.add("empty");
-			}
-		}
-	};
-
-	this.verification = function(index) {
-      return elements[index] && elements[index].innerText === "";
-    };
-
-	this.moveCell = function(neighboringIndex, currenIndex) {
-		elements[neighboringIndex].classList.remove("empty");
-		elements[neighboringIndex].innerText = elements[currenIndex].innerText;
-		elements[currenIndex].innerText = "";
-		elements[currenIndex].classList.add("empty");
-	};
-
-	this.checkSequence = function() {
-		this.winSequences = [123456789101112131415, 159132610143711154812];
-		this.currentArrayCells = "";
-
-		for(var a = 0; a < elements.length; a++){
-			this.currentArrayCells += elements[a].innerText;
-		}
-		if(this.winSequences.indexOf(+this.currentArrayCells) !== -1){
-			cover.classList.add("active");
-			congratulation.classList.add("active");
-			steps.innerText = sumSteps;
-		}
 	};
 }
